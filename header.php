8e66ef99bfcf139876881c1d74f4bc04c0f4a7b6
ALBANO Daniel <deanidev@gmail.com>
2022-10-05 16:20:14 +0200

    home page done  56m50

--- header.php
+++ header.php
@@ -18,19 +18,14 @@
 </head>
 
 <body>
-
     <header class="header text-center">
         <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
-
         <nav class="navbar navbar-expand-lg navbar-dark">
-
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div id="navigation" class="collapse navbar-collapse flex-column">
                 <img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo">
-
                 <ul class="navbar-nav flex-column text-sm-center text-md-left">
                     <li class="nav-item active">
                         <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
@@ -56,7 +51,11 @@
                     <li class="list-inline-item"><a href="#"><i class="fab fa-stack-overflow fa-fw"></i></a></li>
                     <li class="list-inline-item"><a href="#"><i class="fab fa-codepen fa-fw"></i></a></li>
                 </ul>
-
             </div>
         </nav>
     </header>
+    <div class="main-wrapper">
+        <header class="page-title theme-bg-light text-center gradient py-5">
+            <!-- the_title() to show on h1, the title of the post backend -->
+            <h1 class="heading"><?php the_title(); ?></h1>
+        </header>
